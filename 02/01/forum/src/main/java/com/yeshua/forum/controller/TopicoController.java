97debc3c8c3cfa972db346c32e281f127d2f4960
tavares7 <aquilla.tavares7@gmail.com>
2020-11-30 16:45:42 -0300

    1.6 Simplificando a paginação e ordenação

--- 02/01/forum/src/main/java/com/yeshua/forum/controller/TopicoController.java
+++ 02/01/forum/src/main/java/com/yeshua/forum/controller/TopicoController.java
@@ -16,9 +16,9 @@ import com.yeshua.forum.repository.ITopicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,10 +41,9 @@ public class TopicoController {
     private ICursoRepository cursoRepository;
 
     @GetMapping
-    public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina,
-            @RequestParam int qtd, @RequestParam String ordenacao) {
+    public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso,
+            @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
-        Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
         if (nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
