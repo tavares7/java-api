4bab514457cce34ce996a416ce265dcabceeb330
tavares7 <aquilla.tavares7@gmail.com>
2020-12-02 09:45:11 -0300

    2.2 Utilizando cache

--- 02/02/forum/src/main/java/com/yeshua/forum/controller/TopicoController.java
+++ 02/02/forum/src/main/java/com/yeshua/forum/controller/TopicoController.java
@@ -15,6 +15,7 @@ import com.yeshua.forum.repository.ICursoRepository;
 import com.yeshua.forum.repository.ITopicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -41,6 +42,7 @@ public class TopicoController {
     private ICursoRepository cursoRepository;
 
     @GetMapping
+    @Cacheable(value = "listaDeTopicos")
     public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso,
             @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
