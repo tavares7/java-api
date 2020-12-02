180cfe3c1ab92d23a9b72453314ec225170509d6
tavares7 <aquilla.tavares7@gmail.com>
2020-12-02 14:09:04 -0300

    3.6 Restringindo o acesso aos endpoints privados

--- 02/02/forum/src/main/java/com/yeshua/forum/config/security/SecurityConfigurations.java
+++ 02/02/forum/src/main/java/com/yeshua/forum/config/security/SecurityConfigurations.java
@@ -18,7 +18,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll().anyRequest().authenticated().and().formLogin();
     }
 
     @Override
