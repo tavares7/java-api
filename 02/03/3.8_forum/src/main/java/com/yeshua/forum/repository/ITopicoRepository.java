2d52a122763525a3354a8916e38d4f503612f560
tavares7 <aquilla.tavares7@gmail.com>
2020-12-04 11:00:21 -0300

    3.8 Autenticando o usuário

---
+++ 02/03/3.8_forum/src/main/java/com/yeshua/forum/repository/ITopicoRepository.java
@@ -0,0 +1,13 @@
+package com.yeshua.forum.repository;
+
+import com.yeshua.forum.model.Topico;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITopicoRepository extends JpaRepository<Topico, Long> {
+
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
+
+}
