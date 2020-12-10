f4a38a68fa8c558e5354ae43b3dee9857aa2d4e8
tavares7 <aquilla.tavares7@gmail.com>
2020-12-10 17:35:05 -0300

    6.2 Monitoramento com Spring Boot Actuator

--- 02/6_forum/src/main/java/com/yeshua/forum/config/security/SecurityConfigurations.java
+++ 02/6_forum/src/main/java/com/yeshua/forum/config/security/SecurityConfigurations.java
@@ -42,8 +42,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.POST, "/auth").permitAll().anyRequest().authenticated().and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll().antMatchers("/actuator/**").permitAll().anyRequest()
+                .authenticated().and().csrf().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),
                         UsernamePasswordAuthenticationFilter.class);
     }
