e5d33a4ae516759d826f8d1d527fc95e5dc6535a
tavares7 <aquilla.tavares7@gmail.com>
2020-12-22 14:41:21 -0300

    7.5 Enviando token JWT no Swagger

--- 02/7_forum/src/main/java/com/yeshua/forum/config/swagger/SwaggerConfigurations.java
+++ 02/7_forum/src/main/java/com/yeshua/forum/config/swagger/SwaggerConfigurations.java
@@ -1,12 +1,16 @@
 package com.yeshua.forum.config.swagger;
 
+import java.util.Arrays;
+
 import com.yeshua.forum.model.Usuario;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -17,6 +21,8 @@ public class SwaggerConfigurations {
     public Docket forumApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.yeshua.forum")).paths(PathSelectors.ant("/**")).build()
-                .ignoredParameterTypes(Usuario.class);
+                .ignoredParameterTypes(Usuario.class).globalOperationParameters(
+                        Arrays.asList(new ParameterBuilder().name("Authorization").description("Header para token JWT")
+                                .modelRef(new ModelRef("string")).parameterType("header").required(false).build()));
     }
 }
