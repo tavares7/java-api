ad1be9ad44274ec5e988d19ac561c823faaaf150
tavares7 <aquilla.tavares7@gmail.com>
2021-02-10 10:41:45 -0300

    3.6 Testando o repository com outro banco de dados

--- src/test/java/com/yeshua/forum/repository/CursoRespositoryTest.java
+++ src/test/java/com/yeshua/forum/repository/CursoRespositoryTest.java
@@ -6,22 +6,37 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CursoRespositoryTest {
 
     @Autowired
     private CursoRepository repository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void deveriaCarregaCursoAoBuscarNome() {
-        String nome = "HTML 5";
-        Curso curso = repository.findByNome(nome);
+        String cursoNome = "HTML 5";
+
+        Curso cursoHTML5 = new Curso();
+        cursoHTML5.setNome(cursoNome);
+        cursoHTML5.setCategoria("Programação");
+        em.persist(cursoHTML5);
+
+        Curso curso = repository.findByNome(cursoNome);
         Assert.assertNotNull(curso);
-        Assert.assertEquals(nome, curso.getNome());
+        Assert.assertEquals(cursoNome, curso.getNome());
     }
 
     @Test
